f3697fe438c6e295c8815adb272c9c02a06872f6
Tien cookie <abc@m.com>
2025-12-23 21:07:32 +0700

    POM

---
+++ pages/login_page.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.username = (By.NAME, "username")
+        self.password = (By.NAME, "password")
+        self.login_btn = (By.XPATH, "//button[@type='submit']")
+        
+    def enter_username(self, username):
+        self.driver.find_element(*self.username).send_keys(username)
+        
+    def enter_password(self, password):
+        self.driver.find_element(*self.password).send_keys(password)
+        
+    def click_btn_login(self):
+        self.driver.find_element(*self.login_btn).click()
+        
+    def do_login(self, username, password):
+        self.enter_username(username)
+        self.enter_password(password)
+        self.click_btn_login()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    # def enter_username(self,username):
+    #     self.driver1.find_element(By.NAME,)
+        
+    # def enter_password(self,password):
+    #     self.driver1.find_element(By.NAME,)
+
+    # def click_login_button(self):
+    #     self.driver1.find_element(By.XPATH, "//button[@type='submit']").click()
+    
+    # def get_error_message(self):
+    #     return self.driver1.find_element(By.XPATH, "//p[@class='oxd-text oxd-text--p oxd-alert-content-text']']")
