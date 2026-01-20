78b2c88bb7bac97e1d086546392381070caaf508
TienChau <camtien230496@gmail.com>
2026-01-20 20:57:28 +0700

    Github Action

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,9 +9,9 @@ from selenium.webdriver.chrome.service import Service
 @pytest.fixture(scope="function")
 def driver():
     #Đây là setup driver cho các test script dùng & chỉ setup 1 lần duy nhất
-
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new") 
+    options = Options()
+    if headless := ConfigReader.get_headless():
+        options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10) #thời gian chờ tương tác với mỗi text box
     driver.maximize_window()
