5b8121d73f450c4f4aecaf774e95e5f4bd806475
TienChau <camtien230496@gmail.com>
2026-01-20 19:49:30 +0700

    Github Action

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,15 @@
+from pandas import options
 import pytest
 from selenium import webdriver
 from utils.config_reader import ConfigReader
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="function")
 def driver():
     #Đây là setup driver cho các test script dùng & chỉ setup 1 lần duy nhất
 
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new") 
     driver.implicitly_wait(10) #thời gian chờ tương tác với mỗi text box
     driver.maximize_window()
     # base_url = "https://the-internet.herokuapp.com/dropdown"
@@ -21,3 +24,4 @@ def driver():
     #yield: đợi chạy xong TS rồi mới quit
     #Đây là phần teardown cho mỗi test script
     driver.quit()
+
